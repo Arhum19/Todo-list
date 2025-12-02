94456779c09ae131633eb485c41daf365ee84bb0
Arhum19 <arhumnoor10@gmail.com>
2025-12-02 18:43:14 +0500

    starting j unint testing

---
+++ src/test/java/com/example/Todo/repo/TodoRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.example.Todo.repo;
+
+import com.example.Todo.model.todo;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class TodoRepositoryTest {
+
+    @Test
+    void findByCompleted() {
+    }
+
+    @Test
+    void findByTitleContainingIgnoreCase() {
+    }
+
+    @Test
+    void findByUser() {
+    }
+
+    @Test
+    void findByUserId() {
+    }
+
+    @Test
+    void findByUserAndCompleted() {
+    }
+
+    @Test
+    void findByUserIdAndCompleted() {
+    }
+
+    @Test
+    void findByUserAndTitleContainingIgnoreCase() {
+    }
+
+    @Test
+    void findByUserIdAndTitleContainingIgnoreCase() {
+    }
+
+    @Test
+    void testFindByUser() {
+    }
+
+    @Test
+    void testFindByUserAndCompleted() {
+    }
+
+    @Test
+    void testFindByUserAndTitleContainingIgnoreCase() {
+    }
+}
